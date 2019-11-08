3daee72596c7a42f96d5de9c22b8274462144d01
Minhchau <minhchau.dang@liferay.com>
2019-11-07 19:27:25 -0800

    Modify only the list of parents (for breadcrumbs)

--- site/docs/conf.py
+++ site/docs/conf.py
@@ -1,3 +1,6 @@
+import os
+from sphinx.builders.html import StandaloneHTMLBuilder
+
 #
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 #
@@ -24,3 +27,35 @@ release = "1.0"
 source_suffix = [".md", ".rst"]
 templates_path = ["_templates"]
 version = "1.0"
+
+class RelativePathParentsHTMLBuilder(StandaloneHTMLBuilder):
+	def get_doc_context(self, docname, body, metatags):
+		doc_context = super().get_doc_context(docname, body, metatags)
+		doc_context['parents'] = self._get_parents(self.env.titles, docname)
+		return doc_context
+
+	def _get_parents(self, titles, docname):
+		parents = []
+
+		while docname != '/README':
+			basename = os.path.basename(docname)
+
+			if basename == 'README':
+				readme = os.path.dirname(os.path.dirname(docname)) + '/README'
+			else:
+				readme = os.path.dirname(docname) + '/README'
+
+			if readme in titles:
+				parents.append(
+					{
+						'link': self.get_relative_uri(docname, readme),
+						'title': self.render_partial(titles[readme])['title']
+					}
+				)
+
+			docname = readme
+
+		return parents
+
+def setup(app):
+	app.add_builder(RelativePathParentsHTMLBuilder, True)
