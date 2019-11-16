bc60ced018cce4e555d81cbdab534d7739c6cea9
Brian Chan <brian.chan@liferay.com>
2019-11-15 16:34:26 -0800

    Mass compile / SF

--- docs/update_examples.sh
+++ docs/update_examples.sh
@@ -3,4 +3,10 @@
 for dir_name in `find commerce/2.x/en/developer-guide/tutorial -maxdepth 2 -name *.zip -type d`
 do
 	cp -fr commerce/2.x/en/developer-guide/tutorial/_template/* ${dir_name}
+
+	#pushd ${dir_name}
+
+	#./gradlew classes formatSource
+
+	#popd
 done
