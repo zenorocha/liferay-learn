627fb0746551ad05a6442d9702c61a484382060e
Russell Bohl <russell.bohl@liferay.com>
2020-05-08 17:03:21 -0400

    update examples in the whole repo

--- docs/update_examples.sh
+++ docs/update_examples.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for dir_name in `find commerce/2.x/en/developer-guide/tutorials -maxdepth 2 -name *.zip -type d`
+for zip_dir_name in `find . -name "liferay-*.zip" -type d`
 do
-	cp -fr commerce/2.x/en/developer-guide/tutorials/_template/* ${dir_name}
+	cp -fr commerce/2.x/en/developer-guide/tutorials/_template/* ${zip_dir_name}
 
 	#pushd ${dir_name}
 
